e2c5472c0caaefed5176541a5f61042d9fcc3a58
Tyler Durham <TylerDurham@users.noreply.github.com>
2024-08-15 22:24:52 -0500

    Sample weather data, api-key from ENV

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from ascii_art import * 
 from termcolor import cprint, colored
+from config import config
+from dataclasses import dataclass
 
 NEWLINE = '\n'
 
@@ -9,14 +11,31 @@ def current_location():
     
     loc_api = "https://ipinfo.io/loc"
     loc_req = requests.get(loc_api).text
-    lat, long = (float(ll) for ll in loc_req.split(','))
+    lat, long = (float(item) for item in loc_req.split(','))
     return lat, long
 
-lat, long = current_location()
-print(f'Lat: {lat}')
-print(f'Long: {long}')
-quit()
+def call_weather_api():
+    api_key = config["API_KEY"]
+    lat, lon = current_location()
+    weather_api = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&units=imperial&lon={lon}&appid={api_key}"
+    weather_request = requests.get(weather_api).json()
+    return weather_request  
 
+def get_weather():    
+    res = call_weather_api()
+    weather = res["weather"][0]
+    @dataclass
+    class WeatherData:
+        city: str
+        condition_id: int
+        condition: str
+        description: str
+
+    return WeatherData(res["name"], weather["id"], weather["main"], weather["description"])
+
+weather = get_weather()
+print(f'The current conditions in {weather.city} are {weather.condition}')
+quit()
 cprint(NEWLINE.join(sunny), 'yellow')
 cprint(NEWLINE.join(clouds), 'white')
 cprint(NEWLINE.join(rain), 'blue')
